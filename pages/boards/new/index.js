08050645ad3b96264ee49f3d2f6723e1d367e2dd
Ohjinown <dhwlsdnjs55@naver.com>
2024-03-28 16:43:14 +0900

    게시물 등록 페이지 업데이트

--- pages/boards/new/index.js
+++ pages/boards/new/index.js
@@ -4,53 +4,59 @@ export default function BoardInsertPage() {
     
 
     return (
-        <div className={styles.content}>
+        <div className={styles.board_box}>
             {/* 밑에 */}
-            <div className={styles.border}>
-                <div className={styles.border_content}>
-                    <div className={styles.title}>
+            <div className={styles.board}>
+                <div className={styles.board_content}>
+                    <div className={styles.board_title}>
                         <p>게시물 등록</p>
                     </div>
                     <div className={styles.user}>
                         <div className={styles.name}>
                             <label htmlFor="name">작성자</label>
-                            <input id="name" type="text" placeholder="이름을 적어주세요"/>
+                            <input className={styles.boardInput} id="name" type="text" placeholder="이름을 적어주세요"/>
                         </div>
                         <div className={styles.password}>
                             <label htmlFor="password">비밀번호</label>
-                            <input id="password" type="password" placeholder="비밀번호를 입력해주세요"/>
+                            <input className={styles.boardInput} id="password" type="password" placeholder="비밀번호를 입력해주세요"/>
                         </div>
                     </div>
-                    <div>
+                    <div className={styles.title}>
                         <p>제목</p>
-                        <input type="text" placeholder="제목을 입력해주세요"/>
+                        <input className={styles.boardInput} type="text" placeholder="제목을 입력해주세요"/>
                     </div>
-                    <div>
+                    <div className={styles.content}>
                         <p>내용</p>
                         <textarea placeholder="내용을 작성해주세요"></textarea>
                     </div>
-                    <div>
+                    <div className={styles.address_box}>
                         <p>주소</p>
-                        <input type="number"/>
-                        <button>우편번호 검색</button>
-                        <input type="text"/>
-                        <input type="text"/>
+                        <div className={styles.address}>
+                            <input className={styles.address_number} type="number" readOnly/>
+                            <div>
+                                <input className={styles.address_serch_btn} type="button" value="우편번호 검색"/>
+                            </div>
+                        </div>
+                        <div className={styles.address_box_input}>    
+                            <input type="text" readOnly/>
+                            <input type="text" readOnly/>
+                        </div>
                     </div>
-                    <div>
+                    <div className={styles.youtube_box}>
                         <p>유튜브</p>
                         <input type="text" placeholder="링크를 작성해주세요"/>
                     </div>
-                    <div>
+                    <div className={styles.flie_box}>
                         <p>사진 첨부</p>
-                        <div>
-                            <input type="fild"/>
-                            <input type="fild"/>
-                            <input type="fild"/>
+                        <div className={styles.file_box_upload}>
+                            <input type="file"/>
+                            <input type="file"/>
+                            <input type="file"/>
                         </div>
                     </div>
-                    <div>
+                    <div className={styles.main_set}>
                         <p>메인 설정</p>
-                        <div>
+                        <div className={styles.main_set_content}>
                             <input type="radio" id="유튜브"/>
                             <label htmlFor="유튜브">유튜브</label>
                             <input type="radio" id="사진"/>
