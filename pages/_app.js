cf36d93005e598c91c5e07346960f175189a94ab
Ohjinown <dhwlsdnjs55@naver.com>
2024-04-16 13:47:59 +0900

    ; 제거

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
   return (
     
       <ApolloProvider client={client}>
-        <Component />;
+        <Component />
       </ApolloProvider>
     
   )
